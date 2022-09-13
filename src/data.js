e6168179fc49bdef11903d7094120da5bebc6c74
Your Name <drjpolanco25@yahoo.com>
2022-09-13 12:08:48 -0700

    react

---
+++ src/data.js
@@ -0,0 +1,46 @@
+export const projects = [
+  {
+    title: "QUIZMO",
+    subtitle: "SPORTS-QUIZ",
+    description:
+      "TEST YOUR SPORTS KNOWLEDGE",
+    image: "./Capture.PNG",
+    link: "https://jmbm9.github.io/QUIZMO/",
+  },
+  {
+    title: "WORLDS-WEATHER",
+    subtitle: "Check the weather",
+    description:
+      "Use this app to check your local or anywhere for that matter.",
+    image: "./WORLDWEATHER.PNG",
+    link: "https://jmbm9.github.io/WORLDS-WEATHER/",
+  },
+  {
+    title: "Taskinator",
+    subtitle: "Keep track",
+    description:
+      "Stop losing track of your task and use the taskinator",
+    image: "./taskinator.PNG",
+    link: "https://jmbm9.github.io/Taskinator/",
+  },
+  {
+    title: "Runbuddy",
+    subtitle: "Need help?",
+    description:
+      "we have trainers to meet your goal!",
+    image: "./runbuddy.PNG",
+    link: "https://jmbm9.github.io/run-buddy/",
+  },
+  {
+    title: "MoneyWatcha",
+    subtitle: "Track your budget now!",
+    description:
+      "MAKE SURE YOU ARE USING GOOGLE CHROME!",
+    image: "./moneywatcha.PNG",
+    link: "https://moneywatcha.herokuapp.com/",
+  },
+  
+];
+
+
+
